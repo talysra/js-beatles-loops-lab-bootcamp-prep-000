e66ec452135112fe692f48be3719bd99bfd7367c
Stanislava Koklina <medula_oblomgata@mail.ru>
2017-10-04 12:35:47 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,31 @@
+function theBeatlesPlay(musicians, instruments) {
+  var arr = [];
+
+  for (var i = 0; i < musicians.length; i++) {
+    arr.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+
+  return arr;
+}
+
+
+function johnLennonFacts(facts) {
+  var n = 0;
+
+  while (n < facts.length) {
+    facts[n] += "!!!";
+  }
+
+  return facts;
+}
+
+
+function iLoveTheBeatles(number) {
+  var loveArray = [];
+
+  do {
+    loveArray.push("I love the Beatles!")
+  } while (number < 15);
+
+  return loveArray;
+}
